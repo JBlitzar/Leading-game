b0e70596f70a2473dafd1c6ef0c3d42f8c96e24c
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2023-02-09 09:29:32 -0800

    Dying animation

--- Human.gd
+++ Human.gd
@@ -8,7 +8,7 @@ var follownode
 var mounted = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CollisionShape2D.disabled = false
 func dismount():
 	mounted = false
 	follownode.has_passenger = false
@@ -26,10 +26,18 @@ func _on_Human_body_entered(body):
 	print("collide", body.get_collision_layer())
 	if body.get_collision_layer() == 4:
 		#Collided with wood
-		call_deferred("queue_free") #later die fancy
+		DIE()
 	if body.get_collision_layer() == 32769 and not body.has_passenger:
 		#Collided with bouy
 		print("mount on")
 		follownode = body
 		body.has_passenger = true
 		mounted = true
+
+
+func DIE():
+	$AnimationPlayer.play("Die")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	call_deferred("queue_free")
